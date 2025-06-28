a23217a8f3529cc6597c81888d4a34b64be638e8
Eva Isabella Luna <me@crystall1ne.dev>
2025-06-28 17:53:07 -0600

    [FIX] Allow usage of a different NDK location for JDK 8
    This allows users to not need to download the NDK multiple times if they already have it installed.
    
    TODO: Rewrite literally all of these scripts and rebranch.

--- maketoolchain.sh
+++ maketoolchain.sh
@@ -2,9 +2,13 @@
 set -e
 
 . setdevkitpath.sh
-  
-$NDK/build/tools/make-standalone-toolchain.sh \
-	--arch=${TARGET_SHORT} \
-	--platform=android-21 \
-	--install-dir=$NDK/generated-toolchains/android-${TARGET_SHORT}-toolchain
+
+if [[ ! -f $NDK/generated-toolchains/android-${TARGET_SHORT}-toolchain/devkit.info.${TARGET_SHORT} ]]; then
+  $NDK/build/tools/make-standalone-toolchain.sh \
+	  --arch=${TARGET_SHORT} \
+	  --platform=android-21 \
+	  --install-dir=$NDK/generated-toolchains/android-${TARGET_SHORT}-toolchain
 cp devkit.info.${TARGET_SHORT} $NDK/generated-toolchains/android-${TARGET_SHORT}-toolchain/
+else
+  echo "NDK already has android-${TARGET_SHORT}-toolchain."
+fi
--- setdevkitpath.sh
+++ setdevkitpath.sh
@@ -55,7 +55,9 @@ else
 export JVM_PLATFORM=linux
 # Set NDK
 export API=21
-export NDK=`pwd`/android-ndk-$NDK_VERSION
+if [[ -z "$NDK" ]]; then
+  export NDK=`pwd`/android-ndk-$NDK_VERSION
+fi
 export ANDROID_NDK_ROOT=$NDK 
 export TOOLCHAIN=$NDK/generated-toolchains/android-${TARGET_SHORT}-toolchain
 # export TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/linux-x86_64
