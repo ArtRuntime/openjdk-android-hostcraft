62359f04413c3ffe339e652b88a96d9087840cbf
alexytomi <60690056+alexytomi@users.noreply.github.com>
2026-03-03 18:59:35 +0800

    fix: Upstream broke android aarch64 and ios builds
    A "bug fix" makes the android build stop. We could probably just patch
    it out but if this is known to be broken then lets just stay on ga till
    an update is actually needed.
    
    https://github.com/openjdk/jdk8u/commit/ffdc8d8946c307a2ce911b2e6f6fa654c71d81b6
    https://mail.openjdk.org/pipermail/jdk8u-dev/2026-January/020987.html
    
    As for the iOS side
    https://github.com/openjdk/jdk8u/commit/335fce5950cdb8f5e2227019ad3a908432654211#diff-12dec05248e47c97b52087dc8230b8588cdd9c6c85fc5b25b8b9638102729fdaR353
    https://github.com/corretto/corretto-8/commit/335fce5950cdb8f5e2227019ad3a908432654211#diff-12dec05248e47c97b52087dc8230b8588cdd9c6c85fc5b25b8b9638102729fdaR353
    Some freetype changes broke it.

--- clonejdk.sh
+++ clonejdk.sh
@@ -3,7 +3,10 @@ set -e
 if [[ "$TARGET_JDK" == "arm" ]]; then
 git clone --depth 1 https://github.com/openjdk/aarch32-port-jdk8u openjdk
 elif [[ "$BUILD_IOS" == "1" ]]; then
-git clone --depth 1 https://github.com/corretto/corretto-8 openjdk
+git clone --depth 1 https://github.com/corretto/corretto-8 --branch 8.472.08.1 openjdk
+elif [[ "$TARGET_JDK" == "aarch64" ]]; then
+  # See https://github.com/openjdk/jdk8u/commit/ffdc8d8946c307a2ce911b2e6f6fa654c71d81b6 as to how they broke newer versions for aarch64.
+git clone --depth 1 https://github.com/openjdk/jdk8u --branch jdk8u482-ga openjdk
 else
 # Use aarch32 repo because it also has aarch64
 
